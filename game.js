9e670d408dc0b5330c3aeda5473c5013186d749d
Rohit Raj <rohitrajvns2020@gmail.com>
2022-01-27 21:13:26 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 var started = false;
-$(document).on('keydown touchend' ,function () {           // Press any to key to start
+$(document).on('keydown touchend', function () {           // Press any to key to start
     if (!started) {
         setTimeout(function () {                           //Call nextSequence() after a 750 millisecond delay.
+            $("h2").text(" ");
             nextSequence();
         }, 750);
         started = true;
@@ -32,7 +33,7 @@ function nextSequence() {
     sound.play();
 }
 
-$("button").on("click", function () {                                         
+$("button").on("click", function () {
     var userChosenColour = $(this).attr("class");                       // User chosen color        
     userClickedPattern.push(userChosenColour);                          // Pushing the user chosen color in userClickePattern array
     $("." + userChosenColour).fadeOut(150).fadeIn(150);
@@ -44,7 +45,7 @@ $("button").on("click", function () {
     checkAnswer(userClickedPattern.length - 1);                        //check if the most recent user answer is the same as the game pattern              
 });
 function checkAnswer(currentLevel) {
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {   
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         console.log("success");
         if (userClickedPattern.length === gamePattern.length) {             //check that they have finished their sequence with another if statement.
             setTimeout(function () {                                        //Call nextSequence() after a 1 second delay.
@@ -52,9 +53,9 @@ function checkAnswer(currentLevel) {
             }, 1000);
         }
     }
-    else{
+    else {
         // $("h1").text("GAME OVER ");
-        $("h2").text("Your score: " + (Level-1));
+        $("h2").text("Your score: " + (Level - 1));
         var audio = new Audio("sounds/wrong.mp3");
         audio.play();
         $("body").css("background-color", "red");
@@ -62,8 +63,14 @@ function checkAnswer(currentLevel) {
             $("body").css("background-color", "white");
         }, 500);
         $("h1").text("Press any key to restart");
-        $(document).on('keydown touchend', function () {           
-           location.reload();
-        });
+
+        setTimeout(function () {                           //Restart the game.
+            startOver();
+        }, 750);
+        function startOver() {
+            Level = 0;
+            gamePattern = [];
+            started = false;
+        }
     }
 }
