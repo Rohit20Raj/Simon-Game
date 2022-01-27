dcede02f296d58b19daaa8dd20632254bef3a846
Rohit Raj <rohitrajvns2020@gmail.com>
2022-01-27 20:06:21 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var started = false;
 $(document).on('keydown touchend' ,function () {           // Press any to key to start
     if (!started) {
-        setTimeout(function () {                                        //Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {                           //Call nextSequence() after a 750 millisecond delay.
             nextSequence();
         }, 750);
         started = true;
@@ -32,22 +32,22 @@ function nextSequence() {
     sound.play();
 }
 
-$("button").on("click", function () {                                         // User chosen color
-    var userChosenColour = $(this).attr("class");
+$("button").on("click", function () {                                         
+    var userChosenColour = $(this).attr("class");                       // User chosen color        
     userClickedPattern.push(userChosenColour);                          // Pushing the user chosen color in userClickePattern array
     $("." + userChosenColour).fadeOut(150).fadeIn(150);
     playsound(userChosenColour);
-    function playsound(name) {
+    function playsound(name) {                                          // Playing user chosen color sound            
         var audio = new Audio("sounds/" + name + ".mp3");
         audio.play();
     }
-    checkAnswer(userClickedPattern.length - 1);                                      // Playing user chosen color sound
+    checkAnswer(userClickedPattern.length - 1);                        //check if the most recent user answer is the same as the game pattern              
 });
 function checkAnswer(currentLevel) {
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {   //check if the most recent user answer is the same as the game pattern
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {   
         console.log("success");
         if (userClickedPattern.length === gamePattern.length) {             //check that they have finished their sequence with another if statement.
-            setTimeout(function () {                                        //Call nextSequence() after a 1000 millisecond delay.
+            setTimeout(function () {                                        //Call nextSequence() after a 1 second delay.
                 nextSequence();
             }, 1000);
         }
@@ -58,7 +58,7 @@ function checkAnswer(currentLevel) {
         var audio = new Audio("sounds/wrong.mp3");
         audio.play();
         $("body").css("background-color", "red");
-        setTimeout(function () {                                        //Call nextSequence() after a 5000 millisecond delay.
+        setTimeout(function () {                                        //Call nextSequence() after a 500 millisecond delay.
             $("body").css("background-color", "white");
         }, 500);
         $("h1").text("Press any key to restart");
